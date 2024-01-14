738da09fb4cf570b6e178ecd85312a16ef20db6e
Cristóbal Díaz Álvarez <diazalvarezcristobal@gmail.com>
2024-01-14 14:06:10 -0300

    feat: added card front face fade out animation

--- src/app/home/components/card/card.component.ts
+++ src/app/home/components/card/card.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Input } from '@angular/core';
 import { Component } from '@angular/core';
 import { AnyObject, Card } from '../../types';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { TRANSITION_EXIT_PERMANENT } from '../../../app/constants';
 
 /** only displays a card */
 @Component({
@@ -11,6 +13,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [style({ opacity: 1 }), animate(TRANSITION_EXIT_PERMANENT, style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class CardComponent {
   /** card to display */
